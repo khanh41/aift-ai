69e4d7d3706f509b1021e7edf727ca1684c1d1d7
Khánh Pluto <47073675+khanh41@users.noreply.github.com>
2022-01-18 01:08:50 +0700

    🔥 Update draw pose prediction (#2)

--- figures/draw_keypoints.py
+++ figures/draw_keypoints.py
@@ -1,8 +1,5 @@
-import matplotlib.patches as patches
 import numpy as np
 from cv2 import cv2
-from matplotlib import pyplot as plt
-from matplotlib.collections import LineCollection
 
 # Some modules to display an animation using imageio.
 
@@ -72,7 +69,6 @@ def keypoints_and_edges_for_display(keypoints_with_scores,
     """
     keypoints_all = []
     keypoint_edges_all = []
-    edge_colors = []
     num_instances, _, _, _ = keypoints_with_scores.shape
     for idx in range(num_instances):
         kpts_x = keypoints_with_scores[0, idx, :, 1]
@@ -93,85 +89,21 @@ def keypoints_and_edges_for_display(keypoints_with_scores,
                 y_end = kpts_absolute_xy[edge_pair[1], 1]
                 line_seg = np.array([[x_start, y_start], [x_end, y_end]])
                 keypoint_edges_all.append(line_seg)
-                edge_colors.append(color)
     if keypoints_all:
         keypoints_xy = np.concatenate(keypoints_all, axis=0)
     else:
         keypoints_xy = np.zeros((0, 17, 2))
 
-    if keypoint_edges_all:
-        edges_xy = np.stack(keypoint_edges_all, axis=0)
-    else:
-        edges_xy = np.zeros((0, 2, 2))
-    return keypoints_xy, edges_xy, edge_colors
+    return keypoints_xy
 
 
-def draw_prediction_on_image(image, keypoint_locs, keypoint_edges, edge_colors, crop_region=None,
-                             output_image_height=None):
-    """Draws the keypoint predictions on image.
+def draw_prediction_on_image(image, keypoint_locs, points_draw: list, color):
+    """Draws the keypoint predictions on image."""
+    for i in range(len(points_draw)):
+        x1, y1 = keypoint_locs[points_draw[i]].astype(int)
+        cv2.circle(image, (x1, y1), 5, color, 5)
 
-    Args:
-      image: A numpy array with shape [height, width, channel] representing the
-        pixel values of the input image.
-      keypoints_with_scores: A numpy array with shape [1, 1, 17, 3] representing
-        the keypoint coordinates and scores returned from the MoveNet model.
-      crop_region: A dictionary that defines the coordinates of the bounding box
-        of the crop region in normalized coordinates (see the init_crop_region
-        function below for more detail). If provided, this function will also
-        draw the bounding box on the image.
-      output_image_height: An integer indicating the height of the output image.
-        Note that the image aspect ratio will be the same as the input image.
-
-    Returns:
-      A numpy array with shape [out_height, out_width, channel] representing the
-      image overlaid with keypoint predictions.
-    """
-    height, width, channel = image.shape
-    aspect_ratio = float(width) / height
-    fig, ax = plt.subplots(figsize=(12 * aspect_ratio, 12))
-    # To remove the huge white borders
-    fig.tight_layout(pad=0)
-    ax.margins(0)
-    ax.set_yticklabels([])
-    ax.set_xticklabels([])
-    plt.axis('off')
-
-    im = ax.imshow(image)
-    line_segments = LineCollection([], linewidths=4, linestyle='solid')
-    ax.add_collection(line_segments)
-    # Turn off tick labels
-    scat = ax.scatter([], [], s=60, color='#FF1493', zorder=3)
-
-    # (keypoint_locs, keypoint_edges,
-    #  edge_colors) = keypoints_and_edges_for_display(
-    #     keypoints_with_scores, height, width)
-
-    line_segments.set_segments(keypoint_edges)
-    line_segments.set_color(edge_colors)
-    if keypoint_edges.shape[0]:
-        line_segments.set_segments(keypoint_edges)
-        line_segments.set_color(edge_colors)
-    if keypoint_locs.shape[0]:
-        scat.set_offsets(keypoint_locs)
-
-    if crop_region is not None:
-        xmin = max(crop_region['x_min'] * width, 0.0)
-        ymin = max(crop_region['y_min'] * height, 0.0)
-        rec_width = min(crop_region['x_max'], 0.99) * width - xmin
-        rec_height = min(crop_region['y_max'], 0.99) * height - ymin
-        rect = patches.Rectangle(
-            (xmin, ymin), rec_width, rec_height,
-            linewidth=1, edgecolor='b', facecolor='none')
-        ax.add_patch(rect)
-
-    fig.canvas.draw()
-    image_from_plot = np.frombuffer(fig.canvas.tostring_rgb(), dtype=np.uint8)
-    image_from_plot = image_from_plot.reshape(
-        fig.canvas.get_width_height()[::-1] + (3,))
-    plt.close(fig)
-    if output_image_height is not None:
-        output_image_width = int(output_image_height / height * width)
-        image_from_plot = cv2.resize(
-            image_from_plot, dsize=(output_image_width, output_image_height),
-            interpolation=cv2.INTER_CUBIC)
-    return image_from_plot
+        if i < len(points_draw) - 1:
+            x2, y2 = keypoint_locs[points_draw[i + 1]].astype(int)
+            cv2.line(image, (x1, y1), (x2, y2), color, thickness=3)
+    return image
