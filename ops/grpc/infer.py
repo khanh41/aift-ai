65a3db4c55809e1b43c294adbb055de6d3a98069
Khánh Pluto <tranvankhanh99a@gmail.com>
2022-03-12 15:30:49 +0700

    undo

--- ops/grpc/infer.py
+++ ops/grpc/infer.py
@@ -1,12 +1,14 @@
 from base_model.exercise_detect import exercise_detect
 from trainers.exercise_trainer import trainer
-
+import time
 from utils import stringToRGB
 
 
 def image_inference(exercise_code, exercise_name, img_origin):
     img = stringToRGB(img_origin)
+    now = time.time()
     response, score = exercise_detect.detect_image(exercise_code, exercise_name, img)
+    print(time.time() - now)
     return response, str(score)
 
 
